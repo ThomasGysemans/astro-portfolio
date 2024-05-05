392fa11cf61ca114397e098c312adfd13918ebc3
ThomasGysemans <gysemansthomas@gmail.com>
2024-05-05 10:49:00 +0200

    project configuration (tailwind, svelte, etc.)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  site: "https://sciencesky.fr",
+  integrations: [tailwind(), icon(), svelte(), db()]
+});
