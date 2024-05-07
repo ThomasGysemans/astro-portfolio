66cd99b3d9e5064f20411e8e3179ab7afa0db543
ThomasGysemans <gysemansthomas@gmail.com>
2024-05-07 20:45:56 +0200

    Add ThreeJS animation to the showcase of the projects page

---
+++ src/components/svelte/ShowCaseCanvas.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { T, Canvas } from "@threlte/core";
+    import ShowCase from "@components/svelte/ShowCase.svelte";
+</script>
+
+<div class="w-full h-dvh bg-black my-40 overflow-hidden">
+    <Canvas>
+        <T.PerspectiveCamera
+            makeDefault
+            position={[10, 50, 10]}
+            on:create={({ref}) => {
+                ref.lookAt(0, 0, 0);
+            }}
+        />
+
+        <ShowCase />
+    </Canvas>
+</div>
