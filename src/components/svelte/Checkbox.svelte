c309ca6072a54a06072d31b1f2fd2c10a18cf757
ThomasGysemans <gysemansthomas@gmail.com>
2024-05-26 17:57:05 +0200

    add description and checkboxes in the project creation page of the dashboard

---
+++ src/components/svelte/Checkbox.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let name: string;
+
+    let checked = false;
+
+    function toggle() {
+        checked = !checked;
+    }
+</script>
+
+<input type="hidden" {name} value={checked ? "on" : "off"} />
+
+<div class="flex items-center space-x-3">
+    <button class="w-5 h-5 rounded-sm border border-primary/5 {checked ? 'bg-primary-light' : 'bg-white'}" on:click={toggle} />
+    <button type="button" on:click={toggle}>
+        <slot />
+    </button>
+</div>
