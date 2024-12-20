04620c4c295b8a9a5d3d558da0a8af34f3807e7e
Thomas Gysemans <contact@thomasgysemans.dev>
2024-12-20 21:37:24 +0100

    wip: the earth is responsive down to 300px

--- src/components/svelte/Earth.svelte
+++ src/components/svelte/Earth.svelte
@@ -18,52 +18,107 @@
     };
 </script>
 
-<div class="absolute top-6 xl:top-12 -left-[20%] sm:-left-[15%] text-white space-x-2 text-xs">
-    {#each ['2k', '8k'] as r (r)}
-        <button type="button" class="bg-primary rounded-sm py-1 px-2 {res === r ? 'opacity-100' : 'opacity-50 hover:opacity-70'}" on:click={() => res = r}>
-            {r.toUpperCase()}
-        </button>
-    {/each}
+<div class="dialog bg-primary max-w-96 py-2 px-3 rounded-md absolute -top-[14%] -left-[20%] sm:-left-[15%] md:-top-[10%] xl:-top-[16%]">
+    <div class="dialog-triangle absolute -bottom-[10px] xl:-bottom-[20px] right-12 w-0 h-0 border-l-[15px] border-l-transparent border-t-[20px] border-t-primary border-r-[15px] border-r-transparent" />
+    <p class="text-white font-light text-sm xl:text-base">
+        Je suis passionné d’informatique, mais aussi d’astronomie, j’ai ainsi combiné mes passions pour réaliser ce modèle 3D en utilisant
+        <a href="https://threejs.org/" target="_blank" class="underline">ThreeJS</a> et <a href="https://threlte.xyz/" target="_blank" class="underline">Threlte</a> !
+    </p>
+    <div class="resolutions absolute -bottom-10 left-0 text-white space-x-2 text-xs">
+        {#each ['2k', '8k'] as r (r)}
+            <button type="button" class="bg-primary rounded-sm py-1 px-2 {res === r ? 'opacity-100' : 'opacity-50 hover:opacity-70'}" on:click={() => res = r}>
+                {r.toUpperCase()}
+            </button>
+        {/each}
+    </div>
 </div>
 
-<Grabbable>
-    <Canvas>
-        <T.PerspectiveCamera
-            makeDefault
-            position={[10, 10, 10]}
-            on:create={({ref}) => {
-                ref.lookAt(0, 0, 0);
-            }}
-        >
-            <OrbitControls
-                enableDamping
-                enableZoom={false}
-                autoRotate={false}
-                rotateSpeed={0.5}
-            />
-        </T.PerspectiveCamera>
+<div class="earth-container h-full">
+    <Grabbable>
+        <Canvas>
+            <T.PerspectiveCamera
+                makeDefault
+                position={[10, 10, 10]}
+                on:create={({ref}) => {
+                    ref.lookAt(0, 0, 0);
+                }}
+            >
+                <OrbitControls
+                    enableDamping
+                    enableZoom={false}
+                    autoRotate={false}
+                    rotateSpeed={0.5}
+                />
+            </T.PerspectiveCamera>
 
-        <T.DirectionalLight position={[0, 10, 10]} />
-        <T.AmbientLight args={[0xffffff]} intensity={0.1} />
+            <T.DirectionalLight position={[0, 10, 10]} />
+            <T.AmbientLight args={[0xffffff]} intensity={0.1} />
 
-        {#if res === '2k'}
-            <Planet
-                atmosphere
-                {radius}
-                {rotationSpeed}
-                {cloudsRotationSpeed}
-                {tiltRadians}
-                texturesPaths={textures}
-            />
-        {:else}
-            <Planet
-                atmosphere
-                {radius}
-                {rotationSpeed}
-                {cloudsRotationSpeed}
-                {tiltRadians}
-                texturesPaths={textures}
-            />
-        {/if}
-    </Canvas>
-</Grabbable>
+            <!--
+                For some reason, updating the "res" state does not trigger a rebuild of Planet.
+                This condition makes sure that it does.
+            -->
+            {#if res === '2k'}
+                <Planet
+                    atmosphere
+                    {radius}
+                    {rotationSpeed}
+                    {cloudsRotationSpeed}
+                    {tiltRadians}
+                    texturesPaths={textures}
+                />
+            {:else}
+                <Planet
+                    atmosphere
+                    {radius}
+                    {rotationSpeed}
+                    {cloudsRotationSpeed}
+                    {tiltRadians}
+                    texturesPaths={textures}
+                />
+            {/if}
+        </Canvas>
+    </Grabbable>
+</div>
+
+<style lang="scss">
+    // Responsiveness of the dialog boxes requires some highly precise adjustments.
+
+    @media (max-width: 500px) {
+        .earth-container { transform: scale(.85); }
+        .dialog-triangle { right: 80px; }
+    }
+    
+    @media (max-width: 435px) {
+        .dialog { left: -25%; }
+    }
+
+    @media (max-width: 415px) {
+        .dialog > p { font-size: 0.75rem; }
+        .dialog { left: -30%; }
+
+        .earth-container {
+            transform: scale(.75);
+            position: relative;
+            right: -20px;
+        }
+    }
+
+    @media (max-width: 370px) {
+        .earth-container {
+            right: -30px;
+        }
+    }
+
+    @media (max-width: 350px) {
+        .earth-container {
+            transform: scale(.7);
+            right: -50px;
+        }
+    }
+
+    @media (max-width: 320px) {
+        .earth-container { right: -70px; }
+        .dialog { left: -40%; }
+    }
+</style>
