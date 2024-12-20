636485f8cb3b9690bacdb3aa0c90a9bd76b8ca9f
ThomasGysemans <gysemansthomas@gmail.com>
2024-12-20 20:39:45 +0100

    wip: responsiveness of the earth's model

--- src/components/svelte/Grabbable.svelte
+++ src/components/svelte/Grabbable.svelte
@@ -2,6 +2,6 @@
     let grabbing = false;
 </script>
 
-<div class="{grabbing ? 'cursor-grabbing' : 'cursor-grab'} h-full" role="presentation" on:mousedown={() => grabbing = true} on:mouseup={() => grabbing = false}>
+<div class="{grabbing ? 'cursor-grabbing' : 'cursor-grab'} h-full max-md:flex max-md:justify-end" role="presentation" on:mousedown={() => grabbing = true} on:mouseup={() => grabbing = false}>
     <slot />
 </div>
