4d2f557083267a93295f3811262a7b7771ee2fcc
Thomas Gysemans <contact@thomasgysemans.dev>
2024-12-21 11:49:06 +0100

    make sure the pocketbase connexion does not start if there is a missing env variable

--- db/pb.ts
+++ db/pb.ts
@@ -1,3 +1,7 @@
 import PocketBase from 'pocketbase';
 
+if (!import.meta.env.POCKETBASE_URL) {
+    throw new Error("Missing environment variable 'POCKETBASE_URL'");
+}
+
 export const pb = new PocketBase(import.meta.env.POCKETBASE_URL);
